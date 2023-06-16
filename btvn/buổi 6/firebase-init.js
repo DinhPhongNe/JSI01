54aa602f36f232605356b649866526accea1ecb1
DinhPhongNe <phongvo2405@example.com>
2023-06-16 20:45:17 +0700

    update

---
+++ btvn/buổi 6/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZVa63y36euNa6rr63iJABADBevv6Kjrs",
+    authDomain: "fir-anonymous-auth-35be3.firebaseapp.com",
+    projectId: "fir-anonymous-auth-35be3",
+    storageBucket: "fir-anonymous-auth-35be3.appspot.com",
+    messagingSenderId: "608889019483",
+    appId: "1:608889019483:web:1264f904706b3d16e8b37e",
+    measurementId: "G-5JX5DJZLTS"
+  };
